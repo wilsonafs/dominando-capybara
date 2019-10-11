d5ac958fac2fe2e7d8bf4f4a6e5c003529806b02
Fonseca, Priscila Santos Ferreira <priscila.fonseca@wirecard.com>
2019-10-11 19:33:19 -0300

    tratando checkboxes

---
+++ spec/checkboxes_spec.rb
@@ -0,0 +1,25 @@
+describe 'Checkbox', :checkbox do
+    before(:each) do
+        visit('/checkboxes')
+    end
+
+    it 'marcando uma opção' do
+        check('thor')
+    end
+
+    it 'marcando uma opção' do
+        uncheck('antman')
+    end
+
+    it 'marcando com o find set true' do
+        find('input[value=cap]').set(true)
+    end
+
+    it 'desmarcando com o find set false' do
+        find('input[value=guardians]').set(false)
+    end
+
+    after(:each) do
+        sleep 3
+    end
+end
