32f6783c8bf01af942c1f79a1f400bd5718f3e5c
Fonseca, Priscila Santos Ferreira <priscila.fonseca@wirecard.com>
2019-10-11 19:35:54 -0300

    tratando upload de arquivos e imagens

---
+++ spec/upload_spec.rb
@@ -0,0 +1,30 @@
+describe 'Upload de arquivos', :upload do
+    before(:each) do
+       visit('/upload')
+        @arquivo = Dir.pwd + '/spec/fixtures/arquivo.txt'
+        @imagem = Dir.pwd + '/spec/fixtures/imagem.png'
+    end
+
+    it 'upload com arquivo texto' do
+        attach_file('file-upload', @arquivo)
+        click_button 'Upload'
+        
+        div_arquivo = find('#uploaded-file')
+        expect(div_arquivo.text).to eql 'arquivo.txt'
+    end
+
+    it 'upload de imagem', :upload_imagem do
+        attach_file('file-upload', @imagem)
+        click_button 'Upload'
+        
+        imagem = find('#new-image')
+        expect(imagem[:src]).to include '/uploads/imagem.png'
+        
+    end
+
+    after(:each) do
+        sleep 3
+    end
+
+end
+
