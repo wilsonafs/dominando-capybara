fcfc501abcc3ecc65edb9e48472a3065dd2eb826
Fonseca, Priscila Santos Ferreira <priscila.fonseca@wirecard.com>
2019-10-13 17:54:35 -0300

    tentando trabalhar com iframes

---
+++ spec/iframes_spec.rb
@@ -0,0 +1,37 @@
+describe 'Iframes' do
+    describe 'bom', :iframe do
+        
+        before(:each) do
+            visit('/nice_iframe')
+        end
+
+        it 'adicionar ao carrinho' do
+
+            within_frame('burgerId') do 
+                produto = find('.menu-item-info-box', text: 'REFRIGERANTE')
+                produto.find('a').click
+                expect(find('#cart')).to have_content 'Total'
+
+
+            end
+        end
+    end
+
+# end
+
+
+  
+    # describe 'ruim', :bad_iframe do
+    #   before(:each) do
+    #     visit '/bad_iframe'
+    #   end
+  
+    #   it 'carrinho deve estar vazio' do
+    #     script = '$(".box-iframe").attr("id", "tempId");'
+    #     page.execute_script(script)
+  
+    #     within_frame('tempId') do
+    #       expect(find('#cart')).to have_content 'Seu carrinho está vazio!'
+    #     end
+    #   end
+    # end
