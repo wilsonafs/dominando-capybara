81c00e8bd48a200fe1b128b6a7ecf545409a988a
Fonseca, Priscila Santos Ferreira <priscila.fonseca@wirecard.com>
2019-10-14 19:41:17 -0300

    substitui a url padrao que ja consta no spec helper

--- spec/hello_spec.rb
+++ spec/hello_spec.rb
@@ -1,6 +1,6 @@
 describe 'Meu primeiro script' do
     it 'visitar a pagina' do
-        visit 'https://training-wheels-protocol.herokuapp.com'
+        visit '/'
         puts page.title
     end
 
