3122dab3cb4c7d71a217821ab771a21df6ceb340
Fonseca, Priscila Santos Ferreira <priscila.fonseca@wirecard.com>
2019-10-13 22:25:50 -0300

    atualiza os testes de iframe

--- spec/iframes_spec.rb
+++ spec/iframes_spec.rb
@@ -10,28 +10,31 @@ describe 'Iframes' do
             within_frame('burgerId') do 
                 produto = find('.menu-item-info-box', text: 'REFRIGERANTE')
                 produto.find('a').click
-                expect(find('#cart')).to have_content 'Total'
+                expect(find('#cart')).to have_content 'R$ 4,50'
 
 
             end
         end
+
     end
 
-# end
+    describe 'ruim', :bad_iframe do
+        before(:each) do
+            visit '/bad_iframe'
+        end
+  
+        it 'carrinho deve estar vazio' do
+            script = '$(".box-iframe").attr("id", "tempId");'
+            page.execute_script(script)
+  
+            within_frame('tempId') do
+            expect(find('#cart')).to have_content 'Seu carrinho está vazio!'
+            end
+        end
+    end
+
+end
 
 
   
-    # describe 'ruim', :bad_iframe do
-    #   before(:each) do
-    #     visit '/bad_iframe'
-    #   end
-  
-    #   it 'carrinho deve estar vazio' do
-    #     script = '$(".box-iframe").attr("id", "tempId");'
-    #     page.execute_script(script)
-  
-    #     within_frame('tempId') do
-    #       expect(find('#cart')).to have_content 'Seu carrinho está vazio!'
-    #     end
-    #   end
-    # end
+   
